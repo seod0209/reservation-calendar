0aafe6cd32a30ae26a3c02548e08cd99adda8afe
seod0209 <don9zseo@gmail.com>
2024-04-23 13:25:05 +0900

    feat: error boundary 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
 import { ThemeProvider } from '@emotion/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider, useQueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import theme from './styles/theme';
 import GlobalStyle from './styles/Global';
 import Routes from './routes';
 
+import { Button } from './components/button/Button';
+
 const queryClient = new QueryClient();
 
-const App = () => (
-  <div className="App">
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <QueryClientProvider client={queryClient}>
-        <Routes />
-      </QueryClientProvider>
-    </ThemeProvider>
-  </div>
-);
+function fallbackRender({ error, resetErrorBoundary }: { error: any; resetErrorBoundary: any }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: 'red' }}>{error.message}</pre>
+      <Button label="Try again" onClick={() => resetErrorBoundary()} />
+    </div>
+  );
+}
+
+const App = () => {
+  const { reset } = useQueryErrorResetBoundary();
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <QueryClientProvider client={queryClient}>
+          <ErrorBoundary onReset={reset} fallbackRender={fallbackRender}>
+            <Routes />
+          </ErrorBoundary>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </div>
+  );
+};
 
 export default App;
